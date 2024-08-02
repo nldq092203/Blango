fcb15d640714e781f9e8197eadf71b01b7625a28
Quỳnh Nguyễn <nldq220903@gmail.com>
2024-08-02 08:38:31 +0000

    Finish custom template tags

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-  path("", views.index)
+  path("", views.index),
+      path("post/<slug>/", views.post_detail, name="blog-post-detail")
 ]
