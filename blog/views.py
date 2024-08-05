f020b91db4c8816e06bfc338290ed9c1e051fd38
Quỳnh Nguyễn <nldq220903@gmail.com>
2024-08-05 12:41:25 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,11 @@ from blog.forms import CommentForm
 import logging
 
 logger = logging.getLogger(__name__)
+
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
-  logger.debug("Got %d posts", len(posts))
-  return render(request, "blog/index.html", {"posts": posts})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
   
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug=slug)
