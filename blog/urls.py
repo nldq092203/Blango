b3dc3a16c6239a90196bb1324dc6a281918fcc17
Quỳnh Nguyễn <nldq220903@gmail.com>
2024-08-05 16:39:51 +0000

    Finish database optimization

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
   path("", views.index),
-      path("post/<slug>/", views.post_detail, name="blog-post-detail")
+  path("post/<slug>/", views.post_detail, name="blog-post-detail"),
+  path("ip/", views.get_ip)
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
-    logger.debug("Got %d posts", len(posts))
-    return render(request, "blog/index.html", {"posts": posts})
+  posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+  logger.debug("Got %d posts", len(posts))
+  return render(request, "blog/index.html", {"posts": posts})
   
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug=slug)
@@ -33,3 +33,7 @@ def post_detail(request, slug):
   else:
     comment_form = None  
   return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
