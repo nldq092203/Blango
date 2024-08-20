c35c06144b85c498ee1ddeadc54b5485200a9f05
Le NGUYEN <nguyenlediemquynh9a1thd@gmail.com>
2024-08-20 08:46:58 +0000

    Finish First-Party Django REST API

--- blango/settings.py
+++ blango/settings.py
@@ -65,15 +65,14 @@ class Dev(Configuration):
     ]
 
     MIDDLEWARE = [
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        # 'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        "allauth.account.middleware.AccountMiddleware"
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     ]
 
     ROOT_URLCONF = 'blango.urls'
