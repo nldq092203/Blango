7534b548e0ef8d9ef0116cd0f20123c3dcc7a59e
Le NGUYEN <lnguye220903@gmail.com>
2024-08-28 07:11:56 +0000

    Finish JavaScript intro and fundamentals

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
   path("", views.index),
   path("post/<slug>/", views.post_detail, name="blog-post-detail"),
-  path("ip/", views.get_ip)
+  path("ip/", views.get_ip),
+  path("post-table/", views.post_table, name="blog-post-table")
 ]
